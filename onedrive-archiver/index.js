b926100faaad94f5ac46a11fc01ac092aca2225f
gnwzkd <245907549@qq.com>
2017-04-24 01:17:59 +0800

    complete onedrive-archiver

--- onedrive-archiver/index.js
+++ onedrive-archiver/index.js
@@ -2,62 +2,56 @@ const http = require('http');
 const cheerio = require('cheerio');
 const https = require('https');
 const url = require('url');
-
-const pageStr = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>OneLinkr</title>
-</head>
-<body>
-    <p>test.</p>
-</body>
-</html>`;
-
+const fs = require('fs');
 
 http.createServer((req, res) => {
     let urlObj = url.parse(req.url, true);
-    switch (urlObj.path) {
-        case '/':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(pageStr);
-            break;
-        default:
-            let rresid = /[0-9A-F]{16}\!\d{3,4}/g;
-            let resid = urlObj.query.resid;
-            res.writeHead(200, { 'Content-Type': 'text/json' });
-            if (resid && rresid.test(resid)) {
-                https.get(`https://storage.live.com/items/${resid}`, (req) => {
-                    let xml = '';
-                    req.on('data', (data) => {
-                        xml += data;
+    let rresid = /[0-9A-F]{16}\!\d{3,4}/g;
+    let resid = urlObj.query.resid;
+    if (resid && rresid.test(resid)) {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' });
+        https.get(`https://storage.live.com/items/${resid}`, (req) => {
+            let xml = '';
+            req.on('data', (data) => {
+                xml += data;
+            });
+            req.on('end', () => {
+                // console.log(xml);
+                // res.end(xml);
+                // 数据格式
+                // {
+                //     parentResourceId: '5833DC4DF1D8DD1D!105',
+                //     items: [{
+                //         itemType: 'Folder',
+                //         resourceID: '5833DC4DF1D8DD1D!398',
+                //         relationshipName: "Aria2完全配置笔记（Windows）"
+                //     }, {
+                //         itemType: 'Document',
+                //         resourceID: '5833DC4DF1D8DD1D!392',
+                //         relationshipName: "文档1.docx"
+                //     }]
+                // }
+                let $ = cheerio.load(xml);
+                let data = {
+                    parentResourceId: $('ParentResourceID').html(),
+                    items: []
+                };
+                $('Items > *').each((i, v) => {
+                    data.items.push({
+                        itemType: $(v).find('ItemType').html(),
+                        resourceID: $(v).find('ResourceID').html(),
+                        relationshipName: unescape($(v).find('RelationshipName').html().replace(/&#x/g, '%u').replace(/;/g, '').replace(/%uA0/g, ' '))
                     });
-                    req.on('end', () => {
-                        // console.log(xml);
-                        // res.end(xml);
-                        // 数据格式
-                        // [
-                        //     {
-                        //         itemType: 'Folder',
-                        //         resourceID: '5833DC4DF1D8DD1D!398',
-                        //         relationshipName: "Aria2完全配置笔记（Windows）"
-                        //     },
-                        //     {
-                        //         itemType: 'Document',
-                        //         resourceID: '5833DC4DF1D8DD1D!392',
-                        //         relationshipName: "文档1.docx"
-                        //     }
-                        // ]
-                        let data = [];
-                        let $ = cheerio.load(xml);
-                    });
-                }).on('error', (e) => {
-                    console.log(e.message);
-                });;
-            } else {
-                res.end('{}');
-            }
-            break;
+                });
+                res.end(JSON.stringify(data));
+            });
+        }).on('error', (e) => {
+            console.log(e.message);
+        });
+    } else {
+        fs.readFile(urlObj.pathname === '/' ? 'index.html' : urlObj.pathname, (err, data) => {
+            res.end(data);
+        });
     }
 }).listen(3000, '0.0.0.0', () => {
     console.log('Listen in http://0.0.0.0:3000/');
